def5109a67d04d048dcb11ca14862903914f9282
Jake <meh787@gmail.com>
2013-11-11 19:19:40 -0800

    sector length division by numLeds

--- screenchk.py
+++ screenchk.py
@@ -9,7 +9,7 @@ from PIL import Image, ImageGrab
 
 numLeds = 32
 screens = getDisplayRects()
-#Note that screens[0] is the primary screen, regardless of orientation(left/right)
+#Note that screens list places seem to change (screens[0] might not be the monitor you think it is -- once you know which it is it won't change unless you physically change ports)
 
 arduino = serial.Serial('COM12', 9600, timeout=1)
 serial.Serial.flush(arduino)
@@ -32,27 +32,33 @@ def main():
 
 def getSectors(tophalf=False):
 	sector = [] # list of PIL images of sectors of the screen (len should be equal to numLeds)
-	(mainx1, mainy1, mainx2, mainy2) = screens[0]
-	(secx1, secy1, secx2, secy2) = screens[1]
+	(mainx1, mainy1, mainx2, mainy2) = screens[1]
+	(secx1, secy1, secx2, secy2) = screens[0]
 	
 	#need to get the total screen_x length
-	sector_length = ( abs(mainx2) + abs(secx1) ) / (numLeds - 1) #Note: 3520/31=113 and this is not completely correct -- particulary for the last picture
+	sector_length = ( abs(mainx2) + abs(secx1) ) / (numLeds) #Note: 3520/31=110 and this is not completely correct -- particulary for the last picture
 	#may want to experiment with better sector_length values
 	#sector_length = 120
 	div = 1
 	if tophalf:
 		div = 2 # used in last parameter of getarea when appended to sector below
 	
-	halfnumleds = numLeds / 2
+	halfnumLeds = numLeds / 2
 	
 	for x in range(numLeds):
 		# Note that case 14 produces a bad image -- and that the last part of the desktop(rightmost) is lost -- due to inprecision of sector_length
 		# We could handle this case specially... however I think there's a better solution I've yet to find. The effect of this error doesn't bother me.
 		if x * sector_length < abs(secx1): #left screen, screen[1]; a second monitor on the left will have a negative value
-			sector.append(getRectAsImage( (secx1 + (x * sector_length), secy1, secx1 + (x * sector_length) + sector_length, secy2 / div)) )
+			sector.append(getRectAsImage( (secx1 + (x * sector_length), 
+						secy1, 
+						secx1 + (x * sector_length) + sector_length,
+						secy2 / div)) )
 		else: #right (main) screen, screen[0]
 			#note that we can't use same maths as above, seeing as the right screen will start at screen coord 0,0 but x is at numLeds / 2
-			sector.append(getRectAsImage( (0 + ((x - halfnumLeds) * sector_length), 0, 0 + ((x - halfnumLeds) * sector_length) + sector_length,  mainy2 / div)) )
+			sector.append(getRectAsImage( (0 + ((x - halfnumLeds) * sector_length),
+						0, 
+						0 + ((x - halfnumLeds) * sector_length) + sector_length,  
+						mainy2 / div)) )
 		
 	return sector
 
